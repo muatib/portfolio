85b212535b53b02d2253978a5a307b54a17f82e5
dubois <vindubois14@gmail.com>
2024-12-09 21:33:17 +0100

    save

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Portfolio')</title>
+    <link rel="shortcut icon" type="image/png" href="../img/initiales.png"/>
     <script src="https://www.google.com/recaptcha/api.js"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/js/index.js', 'resources/js/projects.js'])
 </head>
